55118edd1d44e7faeed3180d82598444a7977b57
Carolina Nunes Pinheiro <nunespcarolina@gmail.com>
2020-05-15 17:06:49 -0300

    Leitura dos outros dados

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int main() {
 	
-int escolaridade;
+int escolaridade,a;
+char b,c;
 
 cout << "Qual sua escolaridade?" << endl;
 cout << "1) Ensino Fundamental" << endl;
@@ -12,7 +13,14 @@ cout << "2) Ensino Medio" << endl;
 cout << "3) Ensino Superior" << endl;
 cout << "4) Pos-graduacao" << endl;
 cout << "Digite uma opcao: "; 
-cin >> escolaridade;
+cin >>  escolaridade;
+
+cout << "Voce tem quantos anos de expreriencia profissional?";
+cin >> a;
+cout << "Voce tem disponibilidade para viajar? (S/N)";
+cin >> b;
+cout << "Voce tem habilitacao de motorista? (S/N)";
+cin >> c;
 
 	return 0;
 }
