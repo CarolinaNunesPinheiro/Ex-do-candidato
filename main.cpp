0f27cd63278decf4b4c323a821a4fa1378a7edcb
Carolina Nunes Pinheiro <nunespcarolina@gmail.com>
2020-05-16 10:31:11 -0300

    Escolaridade e experiência

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 	
-int escolaridade,a;
+int escolaridade, anos, pontos1, pontos2;
 char b,c;
 
 cout << "Qual sua escolaridade?" << endl;
@@ -16,12 +16,42 @@ cout << "Digite uma opcao: ";
 cin >>  escolaridade;
 
 cout << "Voce tem quantos anos de expreriencia profissional?";
-cin >> a;
+cin >> anos;
 cout << "Voce tem disponibilidade para viajar? (S/N)";
 cin >> b;
 cout << "Voce tem habilitacao de motorista? (S/N)";
 cin >> c;
 
-	return 0;
+if (escolaridade == 1) {
+	pontos1 = 10;
+}
+else if (escolaridade == 2) {
+	pontos1 = 20;
+}
+	else if (escolaridade == 3) {
+	pontos1 = 30;
+}
+else if (escolaridade == 4) {
+	pontos1 = 40;
+}
+
+
+if (anos>5) {
+	pontos2=40;
+}
+else if (anos>2 && anos<=5) {
+pontos2 = 20;
+}
+else if (anos>0 && anos<=2) {
+pontos2 = 10;
+}
+else if (anos==0 ) {
+pontos2 = 0;
+}
+
+cout << "Pontos por escolaridade: " << pontos1 << endl;
+cout << "Pontos por experiencia: " << pontos2 << endl;	
+
+return 0;
 }
 
